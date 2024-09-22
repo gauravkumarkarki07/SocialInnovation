ee6e5d052eccc2c786b887c08b3473feb6675f05
Gaurav <gauravkumarkarki07@gmail.com>
2024-09-22 22:50:35 +1000

    Feat:  Poppins font added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,7 +12,10 @@ export default {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
-  		colors: {}
+  		colors: {},
+      fontFamily:{
+        Poppins:['Poppins','sans-serif']
+      }
   	}
   },
   plugins: [require("tailwindcss-animate")],
