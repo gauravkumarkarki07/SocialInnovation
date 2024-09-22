c81ba45fdf4dade2bc56b540807f55122c9de3d0
Gaurav <gauravkumarkarki07@gmail.com>
2024-09-22 22:41:53 +1000

    Feat: Header added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom"
+import Header from "./Common/views/Header"
 
 function App() {
   return (
-   <Outlet/>
+    <>
+      <Header/>
+      <Outlet />
+    </>
   )
 }
 
---
+++ client/src/Common/views/Header.tsx
@@ -0,0 +1,19 @@
+
+function Header() {
+  return (
+    <section className="flex justify-between">
+        <h1>
+            Social Innovation
+        </h1>
+        <section>
+            <ul className="flex gap-4">
+                <li>Search Bar</li>
+                <li>User Profile</li>
+                <li>Notification</li>
+            </ul>
+        </section>
+    </section>
+  )
+}
+
+export default Header
